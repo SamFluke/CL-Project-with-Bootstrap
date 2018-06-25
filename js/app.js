89f60e2bb80a53c015e8da5d24adda1736b2ee49
Samantha Fluke <Samantha.Fluke@gmail.com>
2018-06-24 21:43:30 -0400

    Audio Working Now

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 // on click event make page play sound  //
+// var x = document.getElementById("myAudio");
+//
+// function playAudio() {
+//     x.play();
+// }
+//
+// function pauseAudio() {
+//     x.pause();
+// }
+
+$(document).ready(function () {
+  RunSetup();
+});
+
+function RunSetup() {
+  $(".audioButton").on("click", function (event) {
+    audios = {
+      "sospBtn": $("#sospCall")[0],
+      "other": "aoasfd"
+    };
+
+    var myAudio = audios[event.target.id];
+    if (myAudio.paused) {
+      myAudio.play();
+    }
+    else {
+      myAudio.pause();
+    }
+  })
+}
